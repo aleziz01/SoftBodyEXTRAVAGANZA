1294274ee6875c6ae13231f144044130bed9358c
aleziz01 <alex.artene@gmail.com>
2024-11-29 23:28:47 +0200

    ost3

--- scripts/musicManager.gd
+++ scripts/musicManager.gd
@@ -2,21 +2,25 @@ extends AudioStreamPlayer
 
 var OST1=preload("res://music/OST1.mp3")
 var OST2=preload("res://music/OST2.mp3")
+var OST3=preload("res://music/OST3.mp3")
 @onready var whatSong: RichTextLabel = $"../Hud/WhatSong"
 
 var rng=RandomNumberGenerator.new()
 var pastNumb=1
-func _ready() -> void:
-	showNewSong()
 
 func _on_finished() -> void:
-	var randNumb=rng.randi_range(1,2)
+	var randNumb=rng.randi_range(1,3)
 	while(randNumb==pastNumb):
-		randNumb=rng.randi_range(1,2)
+		randNumb=rng.randi_range(1,3)
 	if randNumb==1:
 		stream=OST1
+		volume_db=0
 	elif randNumb==2:
 		stream=OST2
+		volume_db=0
+	elif randNumb==3:
+		stream=OST3
+		volume_db=-2
 	pastNumb=randNumb
 	showNewSong()
 	await get_tree().create_timer(3).timeout
@@ -31,3 +35,8 @@ func showNewSong():
 	while(whatSong.position.x>-850):
 		whatSong.position.x-=0.5*int(!global.paused)
 		await get_tree().create_timer(0.01).timeout
+
+
+func _on_play_button_pressed() -> void:
+	_on_finished()
+	showNewSong()
